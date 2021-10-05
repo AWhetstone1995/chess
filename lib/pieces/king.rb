c2be88a4e4ba4614d6d4becadc91afb03ec35e04
Andy Whetstone <andy_whetstone@live.com>
2021-10-05 16:57:08 -0500

    Added king check/checkmate check functionality.

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,4 +1,5 @@
 require_relative 'piece'
+require 'pry-byebug'
 
 class King < Piece
   def initialize(color, location)
@@ -11,12 +12,22 @@ class King < Piece
     moves.each do |move|
       x = @location[0] + move[0]
       y = @location[1] + move[1]
-
+      destination_attacked = destination_in_check?([x, y], board)
       if x.between?(0, 7) && y.between?(0, 7)
-        @possible_moves << [x, y] if board[x][y].nil? || board[x][y].color != @color
+        @possible_moves << [x, y] if (board[x][y].nil? || board[x][y].color != @color) && 
+                                     !destination_attacked
       end
     end
     @possible_moves
+  end
+
+  def destination_in_check?(location, board)
+    board.flatten.each do |piece|
+      next if piece.nil? || piece.color == @color || piece.is_a?(King)
+
+      return true if piece.find_moves(board).include?(location)
+    end
+    false
   end
 
   private
